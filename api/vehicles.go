befb222764d8734395f89c3971171186903b7f8a
Erik Wong <ewong@blizzard.com>
2016-05-04 16:53:54 -0700

    Fix an unclosed connection

--- api/vehicles.go
+++ api/vehicles.go
@@ -31,8 +31,13 @@ func Vehicles(w http.ResponseWriter, req *http.Request) {
 func AllVehicles(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	db, _ := storage.CreateConnection()
-	vd, err := storage.GetVehiclesAfterTime(db, time.Now().Add(time.Minute*-5))
+	db, err := storage.CreateConnection()
+	if err != nil {
+		log.Println("Error connecting to DB in AllVehicles")
+		return
+	}
+	defer db.Close()
+	vd, err := storage.GetVehiclesAfterTime(db, time.Now().Add(time.Minute*-3))
 	compacted := utils.CompactVehicles(vd)
 	out, err := json.Marshal(compacted)
 	if err != nil {
